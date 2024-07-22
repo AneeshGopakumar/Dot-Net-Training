56f96a0cb2dcd70b858d62b3e70d58fab8aa7831
Aneesh <aneesh.gopakumar@ust.com>
2024-07-22 20:37:18 +0530

    Day 3 exercises

---
+++ Day 3/FirstApplication/FirstApplication/Arrays.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day3
+{
+    internal class Arrays
+    {
+        static void ArrSum(int[,] arr1, int[,] arr2) {
+            int[,] arrSum = new int[2, 2];
+            for (int i = 0; i < 2; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    arrSum[i, j] = arr1[i, j] + arr2[i, j];
+                    Console.Write(arrSum[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+        }
+        static void ArrMult(int[,] arr1, int[,] arr2)
+        {
+            int[,] arrMult = new int[2, 2];
+            for (int i = 0; i < 2; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    arrMult[i, j] = arr1[i, j] * arr2[i, j];
+                    Console.Write(arrMult[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+        }
+        static void ArrTranspose(int[,] arr1)
+        {
+            int[,] arrTranspose = new int[2, 2];
+            for (int i = 0; i < 2; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    arrTranspose[i, j] = arr1[j, i] ;
+                    Console.Write(arrTranspose[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+        }
+        static void Main(string[] args)
+        {
+            int[,] arr1 = new int[2, 2] {{ 1, 2 }, { 3, 4 }};
+            int[,] arr2 = new int[2, 2] {{ 5, 6 }, { 7, 8 }};
+            Console.WriteLine("Array Sum - ");
+            ArrSum(arr1 , arr2);
+            Console.WriteLine("Array Multiplication - ");
+            ArrMult(arr1, arr2);
+            Console.WriteLine("Array Transpose - ");
+            ArrTranspose(arr1);
+        }
+    }
+}
---
+++ Day 3/FirstApplication/FirstApplication/Swapping.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day2
+{
+    internal class Swapping
+    {
+        public static void Swap(ref int a, ref int b)
+        {
+            int c = a;
+            a = b;
+            b = c;
+        }
+
+        public static void DiffOut(int a, int b, out int result)
+        {
+            int c = a;
+            a = b;
+            b = c;
+            result = a - b;
+        }
+
+        public static int DiffRef(ref int a, ref int b)
+        {
+            int c = a;
+            a = b;
+            b = c;
+            return(a - b);
+        }
+        static void Main2(string[] args)
+        {
+            int result = 10;
+            Console.WriteLine("Enter 2 nos: ");
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Before swap => a: " + Convert.ToString(a) + "; b: " + Convert.ToString(b));
+            //Swap(ref a, ref b);
+            //Console.WriteLine("After swap => a: " + Convert.ToString(a) + "; b: " + Convert.ToString(b));
+            Console.WriteLine("result- " + result);
+            DiffOut(a, b, out result);
+            Console.WriteLine("DiffOut: a - " + Convert.ToString(a) + " ;b - " + Convert.ToString(b) + " ;Result - " + result);
+            int result2 = DiffRef(ref a, ref b);
+            Console.WriteLine("DiffRef:  a - " + Convert.ToString(a) + " ;b - " + Convert.ToString(b) + " ;Result - " + result2);
+        }
+    }
+}
