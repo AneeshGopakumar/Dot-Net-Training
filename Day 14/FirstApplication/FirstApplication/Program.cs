d9f6aaf7e96c9a1a844dff9694e17b4325ba5301
Aneesh <aneesh.gopakumar@ust.com>
2024-08-12 20:24:56 +0530

    Day 14 exercises

---
+++ Day 14/FirstApplication/FirstApplication/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Formats.Asn1;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading.Tasks;
+using System.Globalization;
+using CsvHelper;
+using CsvHelper.Configuration;
+
+namespace Day_14
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+    }
+    internal class Program
+    {
+        static void Main2(string[] args)
+        {
+            var directory = new DirectoryInfo(AppDomain.CurrentDomain.BaseDirectory).Parent.Parent.Parent;
+            var filePath = directory + "\\csvfiles\\books.csv";
+
+            List<Person> peopleList = new List<Person>
+            {
+                new Person{FirstName="ABC",LastName="XYZ",Age=20},
+                new Person{FirstName="DEF",LastName="UVW",Age=30}
+            };
+
+            using var writer = new StreamWriter(filePath);
+            using var csv = new CsvWriter(writer, new CsvConfiguration(CultureInfo.InvariantCulture));
+            {
+                csv.WriteRecords(peopleList);
+            }
+
+        }
+    }
+}
