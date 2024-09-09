cdc94e6b99b7f5394e0a1fb182435a75581664f3
Aneesh <aneesh.gopakumar@ust.com>
2024-09-09 19:50:57 +0530

    Day 27 win32 controls

---
+++ Day 27 - Win32/DemoWin32/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Runtime.InteropServices;
+namespace DemoWin32
+{
+    internal class Program
+    {
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        public static extern int MessageBox(IntPtr hWnd, string text, string caption, uint type);
+        static void Main2(string[] args)
+        {
+            int a = MessageBox(IntPtr.Zero, "MsgBox Text", "MsgBox Heading", 5);
+            Console.WriteLine(a);
+        }
+    }
+}
---
+++ Day 27 - Win32/DemoWin32/bin/Debug/net6.0/SampleJSFile.js
@@ -0,0 +1,1 @@
+﻿console.log("Test message from JS file");
